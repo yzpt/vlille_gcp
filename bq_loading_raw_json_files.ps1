8a54bea6a804148f0065aed16f4fd2aa300877d4
Yohann <yzpt.de@gmail.Com>
2023-10-30 12:11:50 +0100

    schema in a dir, not tested yet

--- bq_loading_raw_json_files.ps1
+++ bq_loading_raw_json_files.ps1
@@ -12,11 +12,11 @@ $date_inf                                   = "2023-08-25 00:00:00"
 $date_sup                                   = "2023-10-04 00:00:00"
 
 # Temporary tables creation :
-bq mk --table $dataset_name"."$raw_records_table_name json_list_schema_raw_data.json
-bq mk --table $dataset_name"."$transformed_records_from_raw_table_name json_list_schema_records
+bq mk --table $dataset_name"."$raw_records_table_name ./schemas/json_list_schema_raw_data.json
+bq mk --table $dataset_name"."$transformed_records_from_raw_table_name ./schemas/json_list_schema_records
 
 # load raw data from GCS bucket to BigQuery raw_records table
-bq load --source_format=NEWLINE_DELIMITED_JSON $dataset_name"."$raw_records_table_name $raw_json_files_bucket"/*.json" json_list_schema_raw_data.json
+bq load --source_format=NEWLINE_DELIMITED_JSON $dataset_name"."$raw_records_table_name $raw_json_files_bucket"/*.json" ./schemas/json_list_schema_raw_data.json
 # taking almot 150 seconds for 2 months of data (august 25th to october 24th 2023)
 
 # run python job:
