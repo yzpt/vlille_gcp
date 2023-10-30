ebf750a7ca3bcd6b0f930979b5f56c913fc81efb
Yohann <yzpt.de@gmail.Com>
2023-10-30 12:45:15 +0100

    sh for linux, not tested

--- automated_deployment.sh
+++ automated_deployment.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # ======================================================== #
 #                      WORK IN PROGRESS                    #
 # ======================================================== #
@@ -6,45 +8,47 @@
 # gcloud auth login
 
 # 2. Set the project's parameters
-$PROJECT_ID = "zapart-data-vlille"
-$PROJECT_ID
+PROJECT_ID="zapart-data-vlille"
+echo "project_id: "$PROJECT_ID
 
-$REGION = "europe-west9"
-$REGION
+REGION="europe-west9"
+echo "region: "$REGION
 
-$BILLING_ACCOUNT_ID = "011FA7-6A223F-37D9F8"
-$BILLING_ACCOUNT_ID
 # You can retrieve your billing account ID by running the following command:
 # gcloud billing accounts list
+BILLING_ACCOUNT_ID="011FA7-6A223F-37D9F8"
+echo "billing_account_id: "$BILLING_ACCOUNT_ID
 
 
 # ================================================================================
 # Service account parameters
-$SERVICE_ACCOUNT = "SA-" + $PROJECT_ID
-$SERVICE_ACCOUNT_EMAIL = $SERVICE_ACCOUNT + "@" + $PROJECT_ID + ".iam.gserviceaccount.com"
+SERVICE_ACCOUNT="SA-"$PROJECT_ID
+echo "service_account: "$SERVICE_ACCOUNT
+SERVICE_ACCOUNT_EMAIL=$SERVICE_ACCOUNT"@"$PROJECT_ID".iam.gserviceaccount.com"
+echo "service_account_email: "$SERVICE_ACCOUNT_EMAIL
 
 # Docker/GCP Authentication
 # Authentication to Artifact Registry with Docker, auto-confirm
-$artifact_registry_location = $REGION
-gcloud auth configure-docker $artifact_registry_location-docker.pkg.dev --quiet
+ARTIFACT_REGISTRY_LOCATION=$REGION
+gcloud auth configure-docker $ARTIFACT_REGISTRY_LOCATION-docker.pkg.dev --quiet
 
 # Cloud functions parameters
-$FUNCTION_FOLDER = "function"
-$DATA_BUCKET = $PROJECT_ID + "-data"
-$FUNCTION_BUCKET = $PROJECT_ID + "-function"
-$DATASET_ID = "vlille_dataset"
+FUNCTION_FOLDER="function"
+DATA_BUCKET=$PROJECT_ID + "-data"
+FUNCTION_BUCKET=$PROJECT_ID"-function"
+DATASET_ID="vlille_dataset"
 
 # Flask + Docker + Cloud Run parameters
-$dashboard_app_folder = "dashboard_app"
-$artifact_registry_repo_name = "dashboard-vlille"
-$container_name = "dashboard-container"
-$dashboard_service_name = "dashboard-service"
+DASHBOARD_APP_FOLDER="dashboard_app"
+ARTIFACT_REGISTRY_REPO_NAME="dashboard-vlille"
+CONTAINER_NAME="dashboard-container"
+DASHBOARD_SERVICE_NAME="dashboard-service"
 # ================================================================================
 
-# Création d'un nouveau projet gcloud
+# Create new gcloud project
 gcloud projects create $PROJECT_ID
 
-# Activation du projet
+# Activation 
 gcloud config set project $PROJECT_ID
 
 # Attribution du compte de facturation au projet
@@ -54,15 +58,21 @@ gcloud alpha billing projects link $PROJECT_ID --billing-account=$BILLING_ACCOUN
 gcloud iam service-accounts create $SERVICE_ACCOUNT
 
 # Attribution des droits (bigquery admin) au compte de service
-gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:"$SERVICE_ACCOUNT_EMAIL --role="roles/bigquery.admin"
+gcloud projects add-iam-policy-binding \
+    $PROJECT_ID \
+    --member="serviceAccount:"$SERVICE_ACCOUNT_EMAIL \
+    --role="roles/bigquery.admin"
 
 # Attribution des droits (storage admin) au compte de service
-gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:"$SERVICE_ACCOUNT_EMAIL --role="roles/storage.admin"
+gcloud projects add-iam-policy-binding \
+    $PROJECT_ID \
+    --member="serviceAccount:"$SERVICE_ACCOUNT_EMAIL \
+    --role="roles/storage.admin"
 
 # Création d'une clé pour le compte de service
 gcloud iam service-accounts keys create key-$PROJECT_ID.json --iam-account=$SERVICE_ACCOUNT_EMAIL
-cp key-$PROJECT_ID.json $function_folder\key-$PROJECT_ID.json
-cp key-$PROJECT_ID.json $dashboard_app_folder\key-$PROJECT_ID.json
+cp key-$PROJECT_ID.json $FUNCTION_FOLDER\key-$PROJECT_ID.json
+cp key-$PROJECT_ID.json $DASHBOARD_APP_FOLDER\key-$PROJECT_ID.json
 
 
 
@@ -90,21 +100,26 @@ python create_tables.py $PROJECT_ID $DATASET_ID
 
 ### 2.1. Cloud Function :
 
-$pythonFilePath = $FUNCTION_FOLDER + "\main.py"
+PYTHON_FILE_PATH=$FUNCTION_FOLDER + "\main.py"
 
 # Read the content of the Python file
-$content = Get-Content -Path $pythonFilePath
+CONTENT=Get-Content -Path $PYTHON_FILE_PATH
 
 # Replace the specified line with the new project ID
-$newContent = $content -replace 'project_name = .*', "project_name = '$PROJECT_ID'"
-$newContent = $newContent -replace 'bucket_name = .*', "bucket_name = '$DATA_BUCKET'"
-$newContent = $newContent -replace 'dataset_id_from_SH = .*', "dataset_id_from_SH = '$DATASET_ID'"
+NEW_CONTENT = $CONTENT   -replace 'project_name = .*', "project_name = '$PROJECT_ID'"
+NEW_CONTENT = $NEW_CONTENT -replace 'bucket_name = .*', "bucket_name = '$DATA_BUCKET'"
+NEW_CONTENT = $NEW_CONTENT -replace 'dataset_id_from_SH = .*', "dataset_id_from_SH = '$DATASET_ID'"
 
 # Write the modified content back to the Python file
-$newContent | Set-Content -Path $pythonFilePath
+$NEW_CONTENT | Set-Content -Path $PYTHON_FILE_PATH
 
+FUNCTION_FOLDER="function"
+PROJECT_ID="allooo"
 
-Compress-Archive -Path $function_folder\main.py, $function_folder\requirements.txt, $function_folder\key-vlille.json -DestinationPath cf-$PROJECT_ID.zip 
+zip -r cf-$PROJECT_ID.zip \
+    $FUNCTION_FOLDER/main.py \
+    $FUNCTION_FOLDER/requirements.txt \
+    $FUNCTION_FOLDER/key-$PROJECT_ID.json
 
 # Transfert du fichier cf-$PROJECT_ID.zip sur le bucket $FUNCTION_BUCKET
 gsutil cp cf-$PROJECT_ID.zip gs://$FUNCTION_BUCKET
@@ -117,7 +132,7 @@ gcloud pubsub topics create cf-trigger-$PROJECT_ID
 ### 2.3. Job scheduler
   
 # Création d'un job scheduler qui envoie un message au topic Pub/Sub cf-trigger-$PROJECT_ID toutes les minutes
-gcloud scheduler jobs create pubsub cf-$PROJECT_ID-minute --schedule="* * * * *" --topic=cf-trigger-$PROJECT_ID --message-body="{Message du scheduler pubsub cf-$PROJECT_ID-minute}" --time-zone="Europe/Paris" --location=$REGION --description="Scheduler toutes les minutes" --docker-registry=artifact-registry
+gcloud scheduler jobs create pubsub cf-$PROJECT_ID-minute --schedule="* * * * *" --topic=cf-trigger-$PROJECT_ID --message-body="{Message dfrom scheduler pubsub cf-$PROJECT_ID-minute}" --time-zone="Europe/Paris" --location=$REGION --description="Scheduler every minutes" --docker-registry=artifact-registry
 
 
 ### 2.5. Déploiement Cloud Functions
@@ -134,44 +149,60 @@ gcloud functions deploy $PROJECT_ID-scheduled-function --region=$REGION --runtim
 ### 3. Déploiement Flask + Docker sur Cloud Run
 
 # modify the Dockerfile
-$dockerFilePath = $dashboard_app_folder + "\Dockerfile"
+DOCKER_FILE_PATH=$DASHBOARD_APP_FOLDER"\Dockerfile"
 
 # Read the content of the Dockerfile
-$content = Get-Content -Path $dockerFilePath
+CONTENT=Get-Content -Path $DOCKER_FILE_PATH
 
 # Replace the specified line with the new PROJECT_ID and dataset_name
 # CMD ["python3", "app.py", $PROJECT_ID, $DATASET_ID]
-$newLine = 'CMD ["python3", "app.py", "' + $PROJECT_ID + '", "' + $DATASET_ID + '"]'
-$newContent = $content -replace 'CMD .*', $newLine
+NEW_LINE='CMD ["python3", "app.py", "' + $PROJECT_ID + '", "' + $DATASET_ID + '"]'
+NEW_CONTENT=$CONTENT -replace 'CMD .*', $NEW_LINE
 
 # Write the modified content back to the Dockerfile
-$newContent | Set-Content -Path $dockerFilePath
+$NEW_CONTENT | Set-Content -Path $DOCKER_FILE_PATH
 
 # Build Docker image
-docker build -t $artifact_registry_location-docker.pkg.dev/$PROJECT_ID/$artifact_registry_repo_name/$container_name $dashboard_app_folder
+docker build -t \
+    $ARTIFACT_REGISTRY_LOCATION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REGISTRY_REPO_NAME/$CONTAINER_NAME \
+    $DASHBOARD_APP_FOLDER
 
 # Set Artifact Registry administrator permissions for the service account
-gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" --role="roles/artifactregistry.admin"
+gcloud projects add-iam-policy-binding \
+    $PROJECT_ID \
+    --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" \
+    --role="roles/artifactregistry.admin"
 
 # Create a repository on Artifact Registry
-gcloud artifacts repositories create $artifact_registry_repo_name --repository-format=docker --location=$artifact_registry_location --description="Dashboard V'lille"
+gcloud artifacts repositories create \
+    $ARTIFACT_REGISTRY_REPO_NAME \
+    --repository-format=docker \
+    --location=$ARTIFACT_REGISTRY_LOCATION \
+    --description="Dashboard V'lille"
 
 # Push Docker to GCP Artifact Registry
-docker push $artifact_registry_location-docker.pkg.dev/$PROJECT_ID/$artifact_registry_repo_name/$container_name
+docker push $ARTIFACT_REGISTRY_LOCATION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REGISTRY_REPO_NAME/$CONTAINER_NAME
 
 # Cloud Run deployment
 # Enable Cloud Run API
 gcloud services enable run.googleapis.com
 
 # Granting permissions (run admin) to the service account
-gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" --role="roles/run.admin"
+gcloud projects add-iam-policy-binding \
+    $PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" \
+    --role="roles/run.admin"
 
 # Creating a Cloud Run service
-gcloud run deploy $dashboard_service_name --image=$artifact_registry_location-docker.pkg.dev/$PROJECT_ID/$artifact_registry_repo_name/$container_name --region=$REGION --platform=managed --allow-unauthenticated
+gcloud run deploy \
+    $DASHBOARD_SERVICE_NAME \
+    --image=$ARTIFACT_REGISTRY_LOCATION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REGISTRY_REPO_NAME/$CONTAINER_NAME \
+    --region=$REGION \
+    --platform=managed \
+    --allow-unauthenticated
 
 # Logs:
-# gcloud logging read "resource.type=cloud_run_revision AND resource.labels.service_name=$dashboard_service_name" --limit=10
+# gcloud logging read "resource.type=cloud_run_revision AND resource.labels.service_name=$DASHBOARD_SERVICE_NAME" --limit=10
 
 # delete Cloud Run service
-# gcloud run services delete $dashboard_service_name --region=$REGION -q
+# gcloud run services delete $DASHBOARD_SERVICE_NAME --region=$REGION -q
 
